8919db9e42f9608badf088e1bbcee3bce49782f3
Antón <antonmesias@gmail.com>
2024-10-30 18:47:11 +0100

    @Value application.properties 30/10)

--- springboot-web/src/main/java/com/anton/curso/springboot/webbapp/springboot_web/controllers/PathVariableController.java
+++ springboot-web/src/main/java/com/anton/curso/springboot/webbapp/springboot_web/controllers/PathVariableController.java
@@ -3,16 +3,32 @@ package com.anton.curso.springboot.webbapp.springboot_web.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.anton.curso.springboot.webbapp.springboot_web.models.User;
 import com.anton.curso.springboot.webbapp.springboot_web.models.dto.ParamDto;
 
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+
+    @Value("${config.username}")
+    private String username;
+
+    /* @Value("${config.message}")
+    private String message; */
+
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
+
+    @Value("${config.code}")
+    private Integer code;
 
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message){
@@ -30,6 +46,25 @@ public class PathVariableController {
         json.put("product", product);
         json.put("id", id);
 
+        return json;
+    }
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user){
+
+        //hacer algo con el usuario
+
+        return user;
+    }
+
+    @GetMapping("/values")
+    public Map<String, Object> values(@Value("${config.message}") String message){
+
+        Map<String, Object> json = new HashMap<>();
+        json.put("username", username);
+        json.put("message", message);
+        json.put("code", code);
+        json.put("listOfValues", listOfValues);
         return json;
     }
 
